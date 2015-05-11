9c2f6fa91ca92057becee3120df2fdc9ecd0b6a1
Brendan Moran <brendan.moran@arm.com>
2015-05-11 14:49:48 +0100

    Break tests apart and start conversion to support host tests

---
+++ test/tcp_echo_client/tcp_echo_client.cpp
@@ -0,0 +1,71 @@
+/*
+ * PackageLicenseDeclared: Apache-2.0
+ * Copyright (c) 2015 ARM Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "mbed-net-socket-abstract/test/ctest_env.h"
+#include "mbed-net-socket-abstract/test/sal_test_api.h"
+#include "mbed-net-lwip/lwipv4_init.h"
+#include "mbed-net-lwip-eth/EthernetInterface.h"
+#include "mbed/test_env.h"
+
+int main ()
+{
+    MBED_HOSTTEST_TIMEOUT(5);
+    MBED_HOSTTEST_SELECT(default);
+    MBED_HOSTTEST_DESCRIPTION(Socket Abstraction Layer construction and utility test);
+    MBED_HOSTTEST_START("SAL_INIT_UTIL");
+
+    int tests_pass = 1;
+    int rc;
+    char ipbuffer[16];
+
+    struct s_ip_address
+    {
+        int ip_1;
+        int ip_2;
+        int ip_3;
+        int ip_4;
+    } ip_addr = {0, 0, 0, 0};
+    int port = 0;
+
+    printf("MBED: TCPCllient waiting for server IP and port...\r\n");
+    scanf("%d.%d.%d.%d:%d", &ip_addr.ip_1, &ip_addr.ip_2, &ip_addr.ip_3, &ip_addr.ip_4, &port);
+    printf("MBED: Address received: %d.%d.%d.%d:%d\r\n", ip_addr.ip_1, ip_addr.ip_2, ip_addr.ip_3, ip_addr.ip_4, port);
+
+    EthernetInterface eth;
+    /* Initialise with DHCP, connect, and start up the stack */
+    eth.init();
+    eth.connect();
+    printf("TCP client IP Address is %s\r\n", eth.getIPAddress());
+
+    printf("MBED: TCPClient IP Address is %s\r\n", eth.getIPAddress());
+    sprintf(ipbuffer, "%d.%d.%d.%d", ip_addr.ip_1, ip_addr.ip_2, ip_addr.ip_3, ip_addr.ip_4);
+
+    do {
+        socket_error_t err = lwipv4_socket_init();
+        if (!TEST_EQ(err,SOCKET_ERROR_NONE)) {
+            tests_pass = 0;
+            break;
+        }
+        rc = socket_api_test_connect_close(SOCKET_STACK_LWIP_IPV4, SOCKET_AF_INET4, ipbuffer, port);
+        tests_pass = tests_pass && rc;
+
+        rc = socket_api_test_echo_client_connected(SOCKET_STACK_LWIP_IPV4, SOCKET_AF_INET4, SOCKET_STREAM, true, ipbuffer, port);
+        tests_pass = tests_pass && rc;
+    } while (0);
+    notify_completion(tests_pass);
+    return !tests_pass;
+}

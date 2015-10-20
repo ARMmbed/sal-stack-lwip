38a874a1b70d6e3c0e196c520dd5eea314da4547
Brendan Moran <brendan.moran@arm.com>
2015-10-20 17:17:25 +0100

    Added include forwarders

---
+++ mbed-net-lwip/lwip/include/lwip/snmp.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2015, ARM Limited, All Rights Reserved
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef __MBED_NET_LWIP_LWIP_INCLUDE_LWIP_SNMP_H__
+#define __MBED_NET_LWIP_LWIP_INCLUDE_LWIP_SNMP_H__
+#warning mbed-net-lwip/lwip/include/lwip/snmp.h is deprecated.  Please use sal-stack-lwip/lwip/include/lwip/snmp.h instead.
+#include "../../../../sal-stack-lwip/lwip/include/lwip/snmp.h"
+#endif // __MBED_NET_LWIP_LWIP_INCLUDE_LWIP_SNMP_H__
+
